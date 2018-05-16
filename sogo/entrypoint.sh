108af72e1c50975110fdcb77f48bea2335f0f386
Till Wesselmann <till@cryptec.at>
2018-05-16 23:27:00 +0200

    SOGo Webinterface

--- sogo/entrypoint.sh
+++ sogo/entrypoint.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+envsubst < "/etc/sogo/sogo.template.conf" > "/etc/sogo/sogo.conf"
+
 chown sogo:sogo -R /var/lib/sogo/
 
 exec /usr/sbin/sogod -WONoDetach YES -WOPidFile /var/run/sogo/sogo.pid -WOLogFile /dev/stdout
