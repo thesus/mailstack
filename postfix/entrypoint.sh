669305c97903a527a1213f0abe96475f47552f0d
Till Wesselmann <till@cryptec.at>
2018-05-29 17:36:31 +0200

    fixed virtual_transport and removed $mydomain from mydestination 'cause everythings virtual, Removed debug in postfix entrypoint, cleaned up query_filters for ldap lookups

--- postfix/entrypoint.sh
+++ postfix/entrypoint.sh
@@ -5,17 +5,16 @@ set -e
 cp -f /etc/services /var/spool/postfix/etc/services
 cp -f /etc/resolv.conf /var/spool/postfix/etc
 
+cp -a /dev/urandom /var/spool/postfix/dev/
+cp -a /dev/random /var/spool/postfix/dev/
+
 envsubst < "/etc/postfix/ldap_virtual_recipients.template.cf" > "/etc/postfix/ldap_virtual_recipients.cf"
 
 postconf -e "myorigin = $POSTFIX_MYORIGIN"
 postconf -e "virtual_mailbox_domains = $POSTFIX_VIRTUAL_MAILBOX_DOMAINS"
 
-postconf -d | grep mail_version
-
 touch /var/log/mail.log
 
 service rsyslog start
 
-cat /etc/postfix/main.cf
-
-tail -f /var/log/mail.log & exec postfix start-fg
+tail -F /var/log/mail.log & exec postfix start-fg
