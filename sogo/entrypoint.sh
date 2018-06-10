35b349cefd7a677bca124a55ac240a718c4e4716
Till Wesselmann <till@cryptec.at>
2018-06-10 18:22:59 +0200

    Configure sogo with dockerize

--- sogo/entrypoint.sh
+++ sogo/entrypoint.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-envsubst < "/etc/sogo/sogo.template.conf" > "/etc/sogo/sogo.conf"
-
 chown sogo:sogo -R /var/lib/sogo/
 
-exec /usr/sbin/sogod -WONoDetach YES -WOPidFile /var/run/sogo/sogo.pid -WOLogFile /dev/stdout
+exec dockerize -template /etc/sogo/sogo.template.conf:/etc/sogo/sogo.conf \
+    -wait tcp://dovecot:143 -wait tcp://postfix:587 -wait tcp://postgres:5432 -timeout 30s \
+    /usr/sbin/sogod -WONoDetach YES -WOPidFile /var/run/sogo/sogo.pid -WOLogFile /dev/stdout
