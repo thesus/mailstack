7516062a900dcedb2e11c6507417b5000212b96f
Till Wesselmann <till@cryptec.at>
2018-06-10 22:11:48 +0200

    Generate DKIM signature if it doesn't exist

--- rspamd/entrypoint.sh
+++ rspamd/entrypoint.sh
@@ -2,4 +2,16 @@
 
 set -e
 
+
+if [ ! -f /var/lib/rspamd/dkim/2018.key ]; then
+    rspamadm dkim_keygen -b 2018 -s 2018 -k /var/lib/rspamd/dkim/2018.key > /var/lib/rspamd/dkim/2018.txt
+    chown -R _rspamd:_rspamd /var/lib/rspamd/dkim
+    chmod 440 /var/lib/rspamd/dkim/*
+
+    echo -e "\e[31mGenerated a DKIM signature. Put the following DNS Recod in your zone file.\e[0m"
+    cat /var/lib/rspamd/dkim/2018.txt
+else
+    echo "Found DKIM signature, skipping creation."
+fi
+
 exec dockerize -template /var/tmp/rspamd:/etc/rspamd/local.d/ /usr/bin/rspamd -f -u _rspamd -g _rspamd
