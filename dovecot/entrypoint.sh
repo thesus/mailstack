ec97bb791e35d576b2d8f70a1e33e6961d1a8b99
Till Wesselmann <till@cryptec.at>
2018-06-10 16:47:50 +0200

    configure dovecot using dockerize

--- dovecot/entrypoint.sh
+++ dovecot/entrypoint.sh
@@ -4,15 +4,14 @@ set -e
 
 id -u vmail &>/dev/null || useradd -U -M -d /var/vmail vmail
 
-envsubst < "/etc/dovecot/dovecot-ldap.conf.template.ext" > "/etc/dovecot/dovecot-ldap.conf.ext"
-
-chown -R vmail /var/vmail
-chgrp -R vmail /var/vmail
-chmod -R 770 /var/vmail
-
+# 'Compile' Sieve-Script for (global) Spam learning
 sievec /usr/lib/dovecot/sieve/learn-spam.sieve
 sievec /usr/lib/dovecot/sieve/learn-ham.sieve
 sievec /usr/lib/dovecot/sieve/spam-global.sieve
 chmod +x /usr/lib/dovecot/sieve/sa-learn-ham.sh /usr/lib/dovecot/sieve/sa-learn-spam.sh
 
-exec dovecot -F
+chown -R vmail /var/vmail
+chgrp -R vmail /var/vmail
+chmod -R 770 /var/vmail
+
+exec /usr/local/bin/dockerize -template /etc/dovecot/dovecot-ldap.conf.template.ext:/etc/dovecot/dovecot-ldap.conf.ext dovecot -F
