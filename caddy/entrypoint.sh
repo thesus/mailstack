38020ca1ce354836a3551d4c6ba4829892f6f843
Till Wesselmann <till@cryptec.at>
2018-08-29 14:29:01 +0200

    Re-Adding "exec" to entrypoint

--- caddy/entrypoint.sh
+++ caddy/entrypoint.sh
@@ -4,4 +4,4 @@ set -e
 
 export CADDYPATH=/etc/certificates
 
-dockerize -template /etc/Caddyfile.template:/etc/Caddyfile caddy --conf /etc/Caddyfile --log stdout --agree=true
+exec dockerize -template /etc/Caddyfile.template:/etc/Caddyfile caddy --conf /etc/Caddyfile --log stdout --agree=true
