55adefc0d808427aee61870086d35762c6aa0765
Till Wesselmann <till@cryptec.at>
2018-08-30 15:59:52 +0200

    Shell script in entrypoint of ldap utility now supports initializing the (internal) ldap server and adding of new ldifs

--- utils/ldap/entrypoint.sh
+++ utils/ldap/entrypoint.sh
@@ -1,3 +1,28 @@
-/usr/local/bin/dockerize -template /base.template.ldif:/base.ldif -template /services.template.ldif:/services.ldif
+#!/bin/sh
 
-ls /
+if [ -z ${1+x} ]; then
+    cat << EOF
+usage of LDAP-Helper:
+
+init    Create Organizational Units and Users for postfix, dovecot and SoGo
+add     Push ldif into directory. Input via pipe.
+EOF
+    exit 2
+fi
+
+if [ $1 = "init" ]; then
+    echo "Initializing LDAP-Directory..."
+    dockerize -template /base.template.ldif:/base.ldif -template /services.template.ldif:/services.ldif
+
+    ldapadd -D $ADMIN -h ldap:389 -w $ADMIN_PW -f base.ldif
+    ldapadd -D $ADMIN -h ldap:389 -w $ADMIN_PW -f services.ldif
+elif [ $1 = "add" ]; then
+    ldif=`cat`
+
+    echo "Adding...the following ldif"
+    echo "---------------------------------"
+    echo "$ldif"
+    echo "----------------------------------"
+
+    echo "$ldif" | ldapadd -D $ADMIN -h ldap:389 -w $ADMIN_PW
+fi
