5640c0c04c18af354a47fa141cc7c949c5af7957
Till Wesselmann <till@cryptec.at>
2019-04-24 19:47:33 +0200

    Removing tests there is no good reason to keep them in the same repository since they are as complicated as the mailstack itself.

--- test/test_setup.py
+++
@@ -1,9 +0,0 @@
-import pytest #  noqa
-
-def test_containers(compose):
-    # There should be 9 containers in total.
-    assert len(compose.containers) == 9
-
-    for container in compose.containers:
-        container.reload()
-        assert container.status == "running"
