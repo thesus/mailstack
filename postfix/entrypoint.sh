072529552c7b1a02e0059a194da6e72f84649623
David Sieger <dms@iterra-gmbh.de>
2019-09-25 16:32:48 +0200

    Fix custom CA certificates

--- postfix/entrypoint.sh
+++ postfix/entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Copy custom CA certificates to right place and update them
-cp -r /custom-ca-certificates /usr/local/share/ca-certificates
+cp -r /custom-ca-certificates/* /usr/local/share/ca-certificates
 update-ca-certificates
 
 cp -f /etc/services /var/spool/postfix/etc/services
--- sogo/entrypoint.sh
+++ sogo/entrypoint.sh
@@ -2,12 +2,10 @@
 
 set -e
 
-# Copy custom CA certificates to right place and update them
-cp -r /custom-ca-certificates /usr/local/share/ca-certificates
-update-ca-certificates
+sudo /usr/bin/update-ca.sh
 
 chown sogo:sogo -R /var/lib/sogo/
 
-exec dockerize -template /etc/sogo/sogo.template.conf:/etc/sogo/sogo.conf \
-    -wait tcp://dovecot:143 -wait tcp://postfix:587 -wait tcp://postgres:5432 -timeout 30s \
-    /usr/sbin/sogod -WONoDetach YES -WOPidFile /var/run/sogo/sogo.pid -WOLogFile /dev/stdout
+exec su sogo -c "dockerize -template /etc/sogo/sogo.template.conf:/etc/sogo/sogo.conf
+    -wait tcp://dovecot:143 -wait tcp://postfix:587 -wait tcp://postgres:5432 -timeout 30s
+    /usr/sbin/sogod -WONoDetach YES -WOPidFile /var/run/sogo/sogo.pid -WOLogFile /dev/stdout"
