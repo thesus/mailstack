27e6b946af8f9d3ed8d58a1c6ab994aa1f28d29f
David Sieger <dms@iterra-gmbh.de>
2019-09-30 17:13:48 +0200

    Fix permissions for SOGo and add comments about the journey

--- sogo/entrypoint.sh
+++ sogo/entrypoint.sh
@@ -2,10 +2,19 @@
 
 set -e
 
+# While it is counter-intuitive, no way has been found, yet, to make SOGo run,
+# when this container is not started as sogo user. The following approaches have been tryed:
+# - Run container as root, start sogod with su as user sogo
+# - Run container as root, start sogod with sudo as user sogo
+# - Run container as root, start sogod with runuser as user sogo
+# - Experiments with setuid, setgid and Posix ACL's seemed to not work because these settings were not included in Docker images.
+# All approaches resulted in the same thing: sogod exiting with status code 1, not giving away
+# any hint what's wrong. Debugging with gdb showed that the application did not reach its "main" function.
+# Pressed to find a working solution, sogo was allowed to password-less sudo this script:
 sudo /usr/bin/update-ca.sh
 
 chown sogo:sogo -R /var/lib/sogo/
 
-exec su sogo -c "dockerize -template /etc/sogo/sogo.template.conf:/etc/sogo/sogo.conf
-    -wait tcp://dovecot:143 -wait tcp://postfix:587 -wait tcp://postgres:5432 -timeout 30s
-    /usr/sbin/sogod -WONoDetach YES -WOPidFile /var/run/sogo/sogo.pid -WOLogFile /dev/stdout"
+exec dockerize -template /etc/sogo/sogo.template.conf:/etc/sogo/sogo.conf \
+    -wait tcp://dovecot:143 -wait tcp://postfix:587 -wait tcp://postgres:5432 -timeout 30s \
+    /usr/sbin/sogod -WONoDetach YES -WOPidFile /var/run/sogo/sogo.pid -WOLogFile /dev/stdout
--- sogo/update-ca.sh
+++ sogo/update-ca.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# See sogo/entrypoint.sh for more details on why this is a separate file.
+
 # Copy custom CA certificates to right place and update them
 cp -r /custom-ca-certificates/* /usr/local/share/ca-certificates
 update-ca-certificates
